ad29988f18977d30090e2c7745a1cbe1e2ccc744
Omar Masri <3masri.official@gmail.com>
2025-09-17 23:48:03 +0300

    Add argument handling in 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+const { argv } = require('node:process');
+
+if (argv.length === 3) {
+  console.log(argv[2]);
+} else if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
+}
